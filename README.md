b044569479c96a87dbe2807b7e35842c986b6cd4
Codemis <johnathan@pulos.com>
2022-09-09 13:56:44 -0700

    Initial theme code with some content changes.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Personal Portfolio
+
+This is my personal portfolio website that shares more about me and my work.  Check it out live at [https://codemis.com](https://codemis.com).
+
+## Notes
+
+To run this locally, first install the necessary NPM libraries:
+
+```
+npm install --global http-server
+```
+
+Then run it with this command:
+
+```
+npx http-server .
+```
---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//Enable tooltips everywhere
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+});
+/* Github Calendar - https://github.com/IonicaBizau/github-calendar */
+new GitHubCalendar('#github-graph', 'codemis', { responsive: true });
+/* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
+GitHubActivity.feed({ username: 'codemis', selector: '#ghfeed' });
